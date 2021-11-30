99d573d090821794bf39ca2e6c6f0bb74d364e17
Daniele Scasciafratte <mte90net@gmail.com>
2021-11-30 10:58:41 +0100

    fix by @tomjn

--- provision/provision-site.sh
+++ provision/provision-site.sh
@@ -84,9 +84,9 @@ function get_primary_host() {
 function vvv_provision_site_nginx_config() {
   local SITE_NAME=$1
   local SITE_NGINX_FILE=$2
-  if [ ! -f "$SITE_NGINX_FILE" ]; then
-    vvv_warn " * VVV didn't found any Nginx config file, it will use a fallback config file"
-    cp -f "/srv/provision/core/nginx/config/sites/fallback.conf" "$SITE_NGINX_FILE"
+  if [[ -z $NGINX_CONFIGS ]] ; then
+      vvv_error " ! Error: No nginx config was found, VVV will not know how to serve this site"
+      exit 1
   fi
   local DEST_NGINX_FILE=${SITE_NGINX_FILE//\/srv\/www\//}
   local DEST_NGINX_FILE=${DEST_NGINX_FILE//\//\-}
